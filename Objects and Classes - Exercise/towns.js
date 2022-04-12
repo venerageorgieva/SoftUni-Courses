128fd69658d86cd8f8ea113b8bc4e00feb7de483
Venera <102614504+venerageorgieva@users.noreply.github.com>
2022-04-12 20:48:33 +0300

    Add files via upload

---
+++ Objects and Classes - Exercise/towns.js
@@ -0,0 +1,19 @@
+function towns(townList){
+
+   for(let townInfo of townList){
+       let tokens = townInfo.split(' | ');
+       let town = tokens[0];
+       let latitude = Number(tokens[1]).toFixed(2);
+       let longitude = Number(tokens[2]).toFixed(2);
+
+       let townData = {
+        town,
+        latitude,
+        longitude
+       };
+       console.log(townData);
+   } 
+}
+towns(['Sofia | 42.696552 | 23.32601',
+'Beijing | 39.913818 | 116.363625']
+)
